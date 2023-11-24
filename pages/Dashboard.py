7215d1415ee1ac47d163575724573d39a6e729cd
cdmn137 <35605272+cdmn137@users.noreply.github.com>
2023-11-24 15:13:49 -0400

    Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -233,10 +233,6 @@ def show_authenticated_content():
                 (ch).interactive(),
                 use_container_width=True
             )
-        
-        #st.subheader('Todas las comunicaciones')
-        #st.text(f"{len(df)} Casos")
-        #st.dataframe(df)
     ### Fin de Definicion de funciones <------------------------------------------------------
 
     ### -----------------------> Matriz de funcionalidad
